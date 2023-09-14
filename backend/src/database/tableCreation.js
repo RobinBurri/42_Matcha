6a70596cea4b2a85552472397944b42231ec4cad
Robin Burri <robinburri@tutanota.com>
2023-09-14 11:04:07 +0200

    separate execute in dbHelper

--- backend/src/database/tableCreation.js
+++ backend/src/database/tableCreation.js
@@ -1,25 +1,4 @@
-import pkg from 'pg'
-const { Client } = pkg
-
-const execute = async (query) => {
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: 'postgresql',
-        database: process.env.POSTGRES_DB,
-        password: process.env.POSTGRES_PASSWORD,
-        port: process.env.POSTGRES_PORT,
-    })
-    try {
-        await client.connect()
-        await client.query(query)
-        return true
-    } catch (error) {
-        console.error(error.stack)
-        return false
-    } finally {
-        await client.end()
-    }
-}
+import execute from "./dbHelper.js"
 
 const initTables = async () => {
     const userTable = `
